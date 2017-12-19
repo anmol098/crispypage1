29f1f8ae133420777ae10bed385875c7f5f652ba
abhijeet1997 <jaganmohan19972016@gmail.com>
2017-12-19 23:25:19 +0530

    notifications implemented

--- app/build/generated/source/r/debug/android/support/v4/R.java
+++ app/build/generated/source/r/debug/android/support/v4/R.java
@@ -75,28 +75,28 @@ public final class R {
         public static final int actions = 0x7f09001f;
         public static final int async = 0x7f090029;
         public static final int blocking = 0x7f09002d;
-        public static final int cancel_action = 0x7f090033;
-        public static final int chronometer = 0x7f090039;
-        public static final int end_padder = 0x7f090054;
-        public static final int forever = 0x7f09005f;
-        public static final int icon = 0x7f090065;
-        public static final int icon_group = 0x7f090066;
-        public static final int info = 0x7f09006c;
-        public static final int italic = 0x7f09006d;
-        public static final int line1 = 0x7f090072;
-        public static final int line3 = 0x7f090073;
-        public static final int media_actions = 0x7f090078;
-        public static final int normal = 0x7f09008c;
-        public static final int notification_background = 0x7f09008d;
-        public static final int notification_main_column = 0x7f09008e;
-        public static final int notification_main_column_container = 0x7f09008f;
-        public static final int right_icon = 0x7f09009d;
-        public static final int right_side = 0x7f09009e;
-        public static final int status_bar_latest_event_content = 0x7f0900c7;
-        public static final int text = 0x7f0900ce;
-        public static final int text2 = 0x7f0900cf;
-        public static final int time = 0x7f0900d6;
-        public static final int title = 0x7f0900d7;
+        public static final int cancel_action = 0x7f090034;
+        public static final int chronometer = 0x7f09003a;
+        public static final int end_padder = 0x7f090055;
+        public static final int forever = 0x7f090060;
+        public static final int icon = 0x7f090066;
+        public static final int icon_group = 0x7f090067;
+        public static final int info = 0x7f09006d;
+        public static final int italic = 0x7f09006e;
+        public static final int line1 = 0x7f090073;
+        public static final int line3 = 0x7f090074;
+        public static final int media_actions = 0x7f090079;
+        public static final int normal = 0x7f09008d;
+        public static final int notification_background = 0x7f09008e;
+        public static final int notification_main_column = 0x7f09008f;
+        public static final int notification_main_column_container = 0x7f090090;
+        public static final int right_icon = 0x7f09009e;
+        public static final int right_side = 0x7f09009f;
+        public static final int status_bar_latest_event_content = 0x7f0900c8;
+        public static final int text = 0x7f0900cf;
+        public static final int text2 = 0x7f0900d0;
+        public static final int time = 0x7f0900d7;
+        public static final int title = 0x7f0900d8;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f0a0004;
@@ -120,7 +120,7 @@ public final class R {
         public static final int notification_template_part_time = 0x7f0b0045;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0e006d;
+        public static final int status_bar_notification_info_overflow = 0x7f0e006f;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0f0103;
---
+++ app/src/main/java/c/crispypage/app/Config.java
@@ -0,0 +1,18 @@
+package c.crispypage.app;
+
+/**
+ * Created by kjaganmohan on 19/12/17.
+ */
+
+public class Config {
+
+    public static final String TOPIC_GLOBAL = "global";
+
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
